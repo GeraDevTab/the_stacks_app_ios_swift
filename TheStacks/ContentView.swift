96f5834a8930a50f7fe209f1c77ead70bdfd8b5f
Gerardo Custodio <everythingissofast@gmail.com>
2026-05-06 17:31:36 -0600

    adicion de HStack y ZStack ejemplos

--- TheStacks/ContentView.swift
+++ TheStacks/ContentView.swift
@@ -16,8 +16,30 @@ struct ContentView: View {
             Divider()
             Text("VStack Item 3")
         }
-        
         .background(.blue)
+        
+        HStack {
+            Text("HStack Item 1")
+            Divider()
+                .background(.black)
+            Text("HStack Item 2")
+            Divider()
+                .background(.black)
+            Spacer()
+            Text("HStack Item 3")
+        }
+        .background(Color.red)
+        
+        ZStack {
+            Text("ZStack Item 1")
+                .padding()
+                .background(.green)
+                .opacity(0.8)
+            Text("ZStack Item 2")
+                .padding()
+                .background(.green)
+                .offset(x: 80, y: -400)
+        }
     }
 }
 
